015571676a6dec033669c13909d09b92bc323542
Max Humberstone <maxhumberstone@Maxs-MacBook-Pro.local>
2021-04-10 14:45:18 +0100

    log workout - able to log exercises sets for workout

--- src/views/LogWorkout.vue
+++ src/views/LogWorkout.vue
@@ -4,7 +4,7 @@
         <div class="row" v-if="!activeExercise">
             <div class="header-nav-bar">
                 <div><a @click="$router.go(-1)">Cancel</a></div>
-                <div class="float-right">Finish exercise</div>
+                <div class="float-right">Finish workout</div>
             </div>
             <div class="row">
               <div class="col-12">
@@ -18,25 +18,50 @@
                 </div>
               </div>
             </div>
+            <div class="row" v-if="workout.exercises.length > 0">
+              <div class="col-12">
+                <h4>{{workout.exercises.length}} completed exercises</h4>
+                <div class="list" v-for="(exercise, idx) in workout.exercises.slice().reverse()" :key="idx">
+                  {{exercise.name}}
+                  <li v-for="(set, idx) in exercise.sets" :key="idx">
+                    <p>Set {{set.setNumber}} | {{set.reps}} reps of {{set.weight}}kg</p>
+                  </li>
+                </div>
+              </div>
+            </div>
         </div>
 
         <div class="active-exercise" v-if="activeExercise">
           <div class="header-nav-bar">
               <div><a @click="activeExercise = ''">Cancel exercise</a></div>
-              <!-- <div class="float-right">Finish exercise</div> -->
+              <div class="float-right" @click="finishExercise()">Finish exercise</div>
           </div>
           <div class="row">
             <div class="col-12">
               <h2>{{activeExercise.name}}</h2>
               
-              <div class="set">
-                <p>Set 1</p>
-                <div>
-                  <div><span>10</span> reps</div>
-                  <div>*</div>
-                  <div><span>50</span>kg</div>
+              <div class="set active">
+                <p v-if="savedSets">Set {{savedSets.length + 1}}</p>
+                <p v-if="!savedSets">Set 1</p>
+                <div class="reps">
+                  <ion-icon name="remove-outline" class="medium" @click="removeRep()"></ion-icon>
+                  <div><span>{{activeSet.reps}}</span> reps</div>
+                  <ion-icon name="add-outline" class="medium" @click="addRep()"></ion-icon>
                 </div>
-                <button @click="finishSet()">Finish set</button>
+                <div class="input">
+                  <input v-model="activeSet.weight" type="number"/><span class="suffix">kg</span>
+                </div>
+                <button @click="saveNewSet()">Save & new set</button>
+              </div>
+
+              <div class="savedSets">
+                <div class="set" v-for="(set, idx) in savedSets.slice().reverse()" :key="idx">
+                  <p v-if="savedSets">Set {{set.setNumber}}</p>
+                  <div class="reps">
+                    <div><span>{{set.reps}}</span> reps</div>
+                  </div>
+                  <div><span>{{set.weight}}</span> kg</div>
+                </div>
               </div>
 
             </div>
@@ -57,7 +82,15 @@ export default {
       currentExercise: 0,
       currentSet: 0,
       workoutStatus: '',
-      activeExercise: ''
+      activeExercise: '',
+      activeSet: {
+        reps: 10,
+        weight: 50
+      },
+      savedSets:[],
+      workout: {
+        exercises: [],
+      }
     }
   },
   components: {
@@ -73,9 +106,34 @@ export default {
     this.$store.dispatch('getExercises');
   },
   methods: {
+    finishExercise(){
+      const exercise = {
+        name: this.activeExercise.name,
+        exerciseId: this.activeExercise.id,
+        sets: this.savedSets
+      }
+      this.workout.exercises.push(exercise);
+      this.savedSets = [];
+      this.activeExercise = '';
+    },
+    saveNewSet(){
+      const set = {
+        setNumber: this.savedSets.length + 1,
+        reps: this.activeSet.reps,
+        weight: this.activeSet.weight
+      }
+      this.savedSets.push(set);
+    },
+    addRep(){
+      this.activeSet.reps ++;
+    },
+    removeRep(){
+      this.activeSet.reps --;
+    },
     startExercise(exercise){
       console.log(exercise);
       this.activeExercise = exercise;
+      this.searchExercise = '';
     },
     async saveWorkout(workout){
         this.$store.dispatch('saveWorkout', { 
@@ -104,6 +162,7 @@ export default {
 <style lang="scss" scoped>
 .dropdown{
   position: relative;
+  z-index: 1000;
   .list{
     background: $elBackground;
     border-radius: 1rem;
@@ -122,6 +181,28 @@ export default {
         border-radius: 0.5rem;
       }
     }
+  }
+}
+
+.set{
+  background: $elBackground;
+  display: flex;
+  margin-bottom: 1rem;
+  &.active{
+    flex-direction: column;
+    height: 20rem;
+    justify-content: space-around;
+  }
+  justify-content: space-between;
+  align-content: center;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  *{
+    text-align: center;
+  }
+  .reps{
+    display: flex;
+    justify-content: space-around;
   }
 }
 </style>
